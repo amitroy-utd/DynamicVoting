09766d43a47a6b9965a2398b75e64f76e774b8a5
Madhavi Venkatesh <madhavi.venktesh@gmail.com>
2015-04-29 21:30:15 -0500

    Update Application.java

--- src/Application.java
+++ src/Application.java
@@ -1,1 +1,38 @@
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Random;
+
+import org.apache.commons.math3.distribution.ExponentialDistribution;
+
+public class Application implements Serializable {
+		
+	public void sendLockRequest() throws InterruptedException
+	{
+		
+		FileProp fp = new FileProp();
+		String filename;
+		
+		ExponentialDistribution ed= new ExponentialDistribution(fp.mean_delay);
+		fp.readNodeDetails(Project3.CurrentNodeId, Project3.topology);
+		System.out.println("max req is "+fp.num_operations);
+		Random myRandom= new Random();
+		
+		final int ran =myRandom.nextInt(fp.num_operations);
+		if(ran>fp.num_operations*0.8){
+			System.out.println("1");
+			}else{
+				System.out.println("0");	
+			}
+		
+		
+		final int fileran =myRandom.nextInt(fp.num_files);
+		filename = new String(fileran+".txt");
+		
+		Thread.sleep((int)ed.sample());
+	
+	}
+}
+
