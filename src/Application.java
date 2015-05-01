9b67c4ad8d6805c8a2a1f78bd144bed3a9ab3622
Madhavi Venkatesh <madhavi.venktesh@gmail.com>
2015-05-01 17:48:47 -0500

    Update Application.java

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.Serializable;
@@ -9,29 +8,31 @@ import org.apache.commons.math3.distribution.ExponentialDistribution;
 
 public class Application implements Serializable {
 		
-	public void sendLockRequest() throws InterruptedException
+	public void sendRequest() throws InterruptedException
 	{
 		
 		FileProp fp = new FileProp();
+		Protocol pl = new Protocol();
 		String filename;
 		
 		ExponentialDistribution ed= new ExponentialDistribution(fp.mean_delay);
 		fp.readNodeDetails(Project3.CurrentNodeId, Project3.topology);
-		System.out.println("max req is "+fp.num_operations);
+		//System.out.println("max req is "+fp.num_operations);
 		Random myRandom= new Random();
-		
+					
+		for(int i=1;i<=fp.num_operations;i++){
 		final int ran =myRandom.nextInt(fp.num_operations);
-		if(ran>fp.num_operations*0.8){
-			System.out.println("1");
-			}else{
-				System.out.println("0");	
-			}
-		
-		
 		final int fileran =myRandom.nextInt(fp.num_files);
 		filename = new String(fileran+".txt");
-		
+			
+		if(ran>fp.num_operations*0.8){
+			pl.sendLockRequest(1,filename,i,fp.min_wait);
+		}
+		else{
+			pl.sendLockRequest(0,filename,i,fp.min_wait);
+		}
 		Thread.sleep((int)ed.sample());
+	   }
 	
 	}
 }
