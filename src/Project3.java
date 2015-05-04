4a6619b04c11253f3a20a19c4ae2dede61680e7c
krithi-shareth <krithi.shareth@gmail.com>
2015-05-03 19:36:37 -0500

    Update Project3.java

--- src/Project3.java
+++ src/Project3.java
@@ -12,6 +12,8 @@ public class Project3 implements Serializable {
 	static int server_port=0;
 	static String topology="";
 	static Socket socket1;
+	public static TreeMap<Integer, String> treemap = new TreeMap<Integer, String>();
+	public static TreeMap<Integer, String> origTreemap = new TreeMap<Integer, String>();
 	public static void main(String[] args) throws Exception {
 		
 		// get details from file
@@ -43,11 +45,78 @@ public class Project3 implements Serializable {
 				processTaskThread.start();
 				
 				
+			check_all_servers();
         
         
 		
 	}// end of main
 	
+	public static void copyList()
 	
 	
+	{
+		for (Map.Entry<Integer, String> entry : FileProp.map.entrySet())
+		{
+			treemap.put(entry.getKey(), entry.getValue());
+			origTreemap.put(entry.getKey(), entry.getValue());
+		}
+	}
+	
+	public static void check_all_servers() throws IOException
+	{
+		copyList();
+		
+		while(true)
+		{
+			
+			if (treemap.isEmpty())
+			{
+				//call the logic because all the servers are up and running 
+				System.out.println("all servers up continuing. for..."+ Project2.CurrentNodeId);
+				//break out of the loop
+				break;
+			}
+			
+			for (Map.Entry<Integer, String> entry : origTreemap.entrySet())
+		    {
+				boolean remove_entry=false;
+			
+				if (treemap.containsKey(entry.getKey()))
+				{
+					
+					//establish a socket
+					String []nodeNetInfo=entry.getValue().split(":");
+					try
+					{
+						//System.out.println("sending socket to "+ nodeNetInfo[0]+ "  "+ nodeNetInfo[1]);
+						socket1=new Socket(nodeNetInfo[0],Integer.parseInt(nodeNetInfo[1]));
+						MessageStruct ms = new MessageStruct(9,CurrentNodeId);
+						ObjectOutputStream out = null;
+						out = new ObjectOutputStream(socket1.getOutputStream());
+						out.writeObject(ms);
+			           		out.flush();
+			           		out.close();
+						if (socket1.isConnected())
+						{
+							remove_entry = true;
+							
+							//socket1.close();
+						}
+					}
+					catch (Exception e)
+					{
+						//ignore exception
+						//System.out.println("exception is "+e.getMessage());
+					
+						
+					}
+				}
+				if (remove_entry)
+				{
+					System.out.println(entry.getKey() + "is connected and going to remove from treemap");
+					treemap.remove(entry.getKey());
+				}
+		    }
+			
+		}
 } //end of class
